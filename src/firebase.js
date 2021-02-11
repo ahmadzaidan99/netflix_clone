2836ce4ccb42e0dbbeb5ee39b0b08a3867e026a5
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2021-02-11 17:24:33 +0200

    UI

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAqha99SEvKiffh-CSMvl36_94MbQE3v8",
+  authDomain: "netflix-clone-c5d05.firebaseapp.com",
+  projectId: "netflix-clone-c5d05",
+  storageBucket: "netflix-clone-c5d05.appspot.com",
+  messagingSenderId: "648328331200",
+  appId: "1:648328331200:web:4ac7d8fa601fc4727b5225",
+};
